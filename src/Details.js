e6d61e3e493fa5b32276d5b77d059043070d771a
Frank Hollywood <github@srvsync.com>
2021-10-01 16:12:48 -0700

    added pagination and

--- src/Details.js
+++ src/Details.js
@@ -4,34 +4,38 @@ import React, { Component } from 'react'
 import request from 'superagent'
 // import PokemonItem from './PokemonItem'
 import PokemonNew from './PokemonNew'
+import Spinner from './Spinner'
 
 
 export default class Details extends Component {
 
     state = {
-        singlePokemon: {}
+        singlePokemon: {},
+        loading: false
     }
 
     componentDidMount = async() => {
+        this.setState({loading: true})
         const response = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex/${this.props.match.params._id}`)
-        this.setState({singlePokemon: response.body})
-
+        this.setState({
+            singlePokemon: response.body,
+            loading: false
+        })
+        
+     
     }
 
     render() {
         return (
             <div>
-                <PokemonNew />
-                <p>Pokemon Name: {this.state.singlePokemon.pokemon}</p>
-                <p>weight: {this.state.singlePokemon.weight}</p>
-                <p>Type 1: {this.state.singlePokemon.type_1}</p>
-                <p>Type 2: {this.state.singlePokemon.type_2}</p>
-                <p>Attack: {this.state.singlePokemon.attack}</p>
-                <p>Defense: {this.state.singlePokemon.defense}</p>
-                <p>HP: {this.state.singlePokemon.hp}</p>
-                <p>Speed: {this.state.singlePokemon.speed}</p>
-                <p>Hidden Ability: {this.state.singlePokemon.ability_hidden}</p>
-                <img className = "image" src = {this.state.singlePokemon.url_image} alt="pokemon"/>
+                
+                
+                {   
+                this.state.loading
+                ? <Spinner />
+                : <PokemonNew singlePokemon = {this.state.singlePokemon}/>
+                
+            }
             </div>
         )
     }
--- src/PokemonNew.js
+++ src/PokemonNew.js
@@ -4,8 +4,16 @@ export default class PokemonNew extends Component {
     render() {
         return (
             <div>
-               <p>Pokemon Name: {this.props.pokemon}</p> 
-               <img src = {this.props.url_image} alt={this.props.pokemon} />
+                <p>Pokemon Name: {this.props.singlePokemon.pokemon}</p>
+                <p>weight: {this.props.singlePokemon.weight}</p>
+                <p>Type 1: {this.props.singlePokemon.type_1}</p>
+                <p>Type 2: {this.props.singlePokemon.type_2}</p>
+                <p>Attack: {this.props.singlePokemon.attack}</p>
+                <p>Defense: {this.props.singlePokemon.defense}</p>
+                <p>HP: {this.props.singlePokemon.hp}</p>
+                <p>Speed: {this.props.singlePokemon.speed}</p>
+                <p>Hidden Ability: {this.props.singlePokemon.ability_hidden}</p>
+                <img className = "image" src = {this.props.singlePokemon.url_image} alt="pokemon"/>
             </div>
         )
     }
